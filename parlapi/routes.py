47b842e0e230d038b6d0634de338bc0edd519a7b
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-09-20 18:12:42 +0200

    Remove 404 redirect

--- parlapi/routes.py
+++ parlapi/routes.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from flask import redirect
-
 
 def setup_routes(app):
-
-    @app.errorhandler(404)
-    def error404(e):
-        return redirect('/')
-
     @app.route('/')
     def hello():
         return 'Hi!'
