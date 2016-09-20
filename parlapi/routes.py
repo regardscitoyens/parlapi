037f75932c62f8175bea8a14370acc9833fdebe9
Nicolas Joyard <joyard.nicolas@gmail.com>
2016-09-20 22:18:07 +0200

    Add homepage

--- parlapi/routes.py
+++ parlapi/routes.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from flask import abort, jsonify
+from flask import render_template, url_for
+
+from .models import Job
 
 
 def setup_routes(app, api):
-    api.setup_routes(app)
+    api_prefix = '/api/'
+    api.setup_routes(app, api_prefix)
 
     @app.route('/')
-    def hello():
-        return 'Bienvenue sur ParlAPI :)'
+    def home():
+        jobs = Job.query.all()
+        return render_template('index.html', api=api.descriptions,
+                               api_prefix=api_prefix, jobs=jobs,
+                               logo=url_for('static', filename='parlapi.png'))
